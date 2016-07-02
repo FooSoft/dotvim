465fbd00ef15d752d609203d442838b735c966db
Alex Yatskov <alex@foosoft.net>
2016-07-02 12:00:33 -0700

    Moving location of installer

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # YouCompleteMe
-pushd ./bundle/YouCompleteMe
+pushd .vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 ./install.py
 popd
 
 # tern_for_vim
-pushd ./bundle/tern_for_vim
+pushd .vim/bundle/tern_for_vim
 sudo npm install -g tern
 npm install
 popd
 
 # syntastic
 sudo npm -g install eslint
-sudo pip install pyflakes
+sudo pip install pyflakes --upgrade
