42d407ea8f6651888e3dc8f6f6b0f1bdb6d3e61e
Alex Yatskov <alex@foosoft.net>
2019-04-20 14:47:39 -0700

    update plugins

--- install.sh
+++ install.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-# YouCompleteMe
-pushd .vim/bundle_extra/YouCompleteMe
 git submodule update --init --recursive
-./install.py --clang-completer
-popd
-
-# tern_for_vim
-pushd .vim/bundle_extra/tern_for_vim
-sudo npm install -g tern
-npm install
-popd
-
-# ale
-sudo npm -g install eslint
 pip install --user pyflakes --upgrade
